598c8e9281ffce75f4dd69fb7c1b90778138ba18
jtomassoni <jtomassoni@gmail.com>
2020-12-22 14:08:42 -0700

    further form testing

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,6 +9,7 @@ import {
 	Form,
 	Label,
 	InputTextArea,
+	InputText,
 	ColumnTwo,
 	Img,
 	ImgContainer,
@@ -20,52 +21,79 @@ const Contact = () => {
 	const [name, setName] = useState();
 	const [email, setEmail] = useState();
 
+	// const encode = (data) => {
+	// 	return Object.keys(data)
+	// 		.map(
+	// 			(key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+	// 		)
+	// 		.join('&');
+	// };
+
+	// const handleSubmit = (e) => {
+	// 		fetch('/', {
+	// 			method: 'POST',
+	// 			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+	// 			body: encode({ 'form-name': 'contact', ...this.state }),
+	// 		})
+	// 			.then(() => alert('Success!'))
+	// 			.catch((error) => alert(error));
+
+	// 		e.preventDefault();
+	// 	};
+
 	return (
 		<Container altBg={true} id='contact'>
-			<Wrapper>
-				<Row imgPosition={true}>
-					<ColumnOne>
-						<Form name='contact-form' method='POST' netlify>
-							<h1>Contact Me:</h1>
-							<Label htmlFor='name'>Name:</Label>
-							<InputTextArea
-								type='text'
-								name='name'
-								id='name'
-								form='nameform'
-								onChange={(event) => setName(event.target.value)}
-								value={name}
-							/>
-							<Label htmlFor='email'>Email:</Label>
-							<InputTextArea
-								type='email'
-								name='email'
-								id='email'
-								form='emailform'
-								onChange={(event) => setEmail(event.target.value)}
-								value={email}
-							/>
-							<Label htmlFor='additionalDetails'>Comments:</Label>
-							<InputTextArea
-								type='text'
-								rows='8'
-								name='details'
-								id='details'
-								form='detailsform'
-								onChange={(event) => setContact(event.target.value)}
-								value={contact}
-							/>
-							<FormButton>Submit</FormButton>
-						</Form>
-					</ColumnOne>
-					<ColumnTwo>
-						<ImgContainer>
-							<Img src={contactpic} alt='contact' />
-						</ImgContainer>
-					</ColumnTwo>
-				</Row>
-			</Wrapper>
+			<form name='contactform' method='POST' netlify>
+				<input name='contact' type='text'></input>
+				<button>Submit</button>
+			</form>
 		</Container>
+
+		// <Container altBg={true} id='contact'>
+		// 	<Wrapper>
+		// 		<Row imgPosition={true}>
+		// 			<ColumnOne>
+		// 				<Form type='hidden' name='contact-form' netlify onSubmit={handleSubmit}>
+		// 					<h1>Contact Me:</h1>
+		// 					<Label htmlFor='name'>Name:</Label>
+		// 					<InputText
+		// 						type='text'
+		// 						name='name'
+		// 						id='name'
+		// 						// form='nameform'
+		// 						onChange={(event) => setName(event.target.value)}
+		// 						value={name}
+		// 					/>
+		// 					<Label htmlFor='email'>Email:</Label>
+		// 					<InputText
+		// 						type='email'
+		// 						name='email'
+		// 						id='email'
+		// 						// form='emailform'
+		// 						onChange={(event) => setEmail(event.target.value)}
+		// 						value={email}
+		// 					/>
+		// 					<Label htmlFor='additionalDetails'>Comments:</Label>
+		// 					<InputTextArea
+		// 						type='text'
+		// 						rows='8'
+		// 						name='details'
+		// 						id='details'
+		// 						// form='detailsform'
+		// 						onChange={(event) => setContact(event.target.value)}
+		// 						value={contact}
+		// 					/>
+		// 					<FormButton>Submit</FormButton>
+		// 				</Form>
+		// 			</ColumnOne>
+		// 			<ColumnTwo>
+		// 				<ImgContainer>
+		// 					<Img src={contactpic} alt='contact' />
+		// 				</ImgContainer>
+		// 			</ColumnTwo>
+		// 		</Row>
+		// 	</Wrapper>
+		// </Container>
 	);
 };
 
