dffba24b6ed9e873c1d349b935bbad1a0ba1ee5e
jtomassoni <jtomassoni@gmail.com>
2020-12-22 14:32:28 -0700

    trying to deploy on netlify v2

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-// import contactpic from '../../images/icons/contact.svg';
+import contactpic from '../../images/icons/contact.svg';
 
-// import {
-// 	Container,
-// 	Wrapper,
-// 	Row,
-// 	ColumnOne,
-// 	Form,
-// 	Label,
-// 	InputTextArea,
-// 	InputText,
-// 	ColumnTwo,
-// 	Img,
-// 	ImgContainer,
-// 	FormButton,
-// } from './ContactElements';
+import {
+	Container,
+	Wrapper,
+	Row,
+	ColumnOne,
+	Form,
+	Label,
+	InputTextArea,
+	InputText,
+	ColumnTwo,
+	Img,
+	ImgContainer,
+	FormButton,
+} from './ContactElements';
 
 const Contact = () => {
 	const [contact, setContact] = useState();
@@ -22,52 +22,47 @@ const Contact = () => {
 	const [email, setEmail] = useState();
 
 	return (
-		<form name='example' method='POST' netlify>
-			<input name='something' type='text'></input>
-			<button>submit</button>
-		</form>
-
-		// <Container altBg={true} id='contact'>
-		// 	<Wrapper>
-		// 		<Row imgPosition={true}>
-		// 			<ColumnOne>
-		// 				<Form>
-		// 					<h1>Contact Me:</h1>
-		// 					<Label htmlFor='name'>Name:</Label>
-		// 					<InputText
-		// 						type='text'
-		// 						name='name'
-		// 						onChange={(event) => setName(event.target.value)}
-		// 						value={name}
-		// 					/>
-		// 					<Label htmlFor='email'>Email:</Label>
-		// 					<InputText
-		// 						type='email'
-		// 						name='email'
-		// 						onChange={(event) => setEmail(event.target.value)}
-		// 						value={email}
-		// 					/>
-		// 					<Label htmlFor='additionalDetails'>Comments:</Label>
-		// 					<InputTextArea
-		// 						type='text'
-		// 						rows='8'
-		// 						name='details'
-		// 						id='details'
-		// 						form='detailsform'
-		// 						onChange={(event) => setContact(event.target.value)}
-		// 						value={contact}
-		// 					/>
-		// 					<FormButton>Submit</FormButton>
-		// 				</Form>
-		// 			</ColumnOne>
-		// 			<ColumnTwo>
-		// 				<ImgContainer>
-		// 					<Img src={contactpic} alt='contact' />
-		// 				</ImgContainer>
-		// 			</ColumnTwo>
-		// 		</Row>
-		// 	</Wrapper>
-		// </Container>
+		<Container altBg={true} id='contact'>
+			<Wrapper>
+				<Row imgPosition={true}>
+					<ColumnOne>
+						<Form>
+							<h1>Contact Me:</h1>
+							<Label htmlFor='name'>Name:</Label>
+							<InputText
+								type='text'
+								name='name'
+								onChange={(event) => setName(event.target.value)}
+								value={name}
+							/>
+							<Label htmlFor='email'>Email:</Label>
+							<InputText
+								type='email'
+								name='email'
+								onChange={(event) => setEmail(event.target.value)}
+								value={email}
+							/>
+							<Label htmlFor='additionalDetails'>Comments:</Label>
+							<InputTextArea
+								type='text'
+								rows='8'
+								name='details'
+								id='details'
+								form='detailsform'
+								onChange={(event) => setContact(event.target.value)}
+								value={contact}
+							/>
+							<FormButton>Submit</FormButton>
+						</Form>
+					</ColumnOne>
+					<ColumnTwo>
+						<ImgContainer>
+							<Img src={contactpic} alt='contact' />
+						</ImgContainer>
+					</ColumnTwo>
+				</Row>
+			</Wrapper>
+		</Container>
 	);
 };
 
