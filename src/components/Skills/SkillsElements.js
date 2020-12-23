5adc1428b7a50d47b36aafa01c77df9996b8c5c3
jtomassoni <jtomassoni@gmail.com>
2020-12-22 19:05:05 -0700

    responsiveness tweaks

--- src/components/Skills/SkillsElements.js
+++ src/components/Skills/SkillsElements.js
@@ -47,7 +47,7 @@ export const SkillsCard = styled.div`
 	align-items: center;
 	border-radius: 10px;
 	max-height: 400px;
-	width: 200px;
+	width: 300px;
 	padding: 30px;
 	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
 	transition: all 0.2s ease-in-out;
